9141acad6c48a1edb1451d221037c65b51633000
dimarudenk <dimarudenk@gmail.com>
2019-01-30 22:35:00 +0300

    add gmail services

---
+++ src/main/java/ru/drudenko/alisa/service/google/Main.java
@@ -0,0 +1,27 @@
+package ru.drudenko.alisa.service.google;
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            GmailService gmailService = new GmailServiceImpl(GoogleNetHttpTransport.newTrustedTransport());
+            gmailService.setGmailCredentials(GmailCredentials.builder()
+                    .userEmail("YOUR_EMAIL@gmail.com")
+                    .clientId("1078329436949-rspqf1hkbedrqavvcakn8k6l6qd9asnl.apps.googleusercontent.com")
+                    .clientSecret("7Axm_PMdL3lppLdkA-MmPo7h")
+                    .accessToken("ya29.GluCBY6YE-TzEU2-F86sRl_Gq5QyPmUNW2wEV0MynFN-L3HK2AHEUD09pknXfrvk8UY6NYnGwuCIxAh97s6ipVylgwoNIsxLs7uouIBqj8vWiAODGiS2a1ZDNa8D")
+                    .refreshToken("1/XyMnZb4UfU8WDt6SnHIeE3wFTPyTAg2K16ZA7NIF0bY")
+                    .build());
+
+            gmailService.sendMessage("RECIPIENT_EMAIL@gmail.com", "Subject", "body text");
+        } catch (GeneralSecurityException | IOException | MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
