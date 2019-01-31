8f725d624c4bfd6086468e874d272e65be077b34
drudenko <dimarudenk@gmail.com>
2019-01-31 19:42:16 +0300

    gmail

--- src/main/java/ru/drudenko/alisa/controller/OauthController.java
+++ src/main/java/ru/drudenko/alisa/controller/OauthController.java
@@ -1,5 +1,16 @@
 package ru.drudenko.alisa.controller;
 
+import com.google.api.client.auth.oauth.OAuthParameters;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.Label;
+import com.google.api.services.gmail.model.ListLabelsResponse;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
@@ -24,16 +35,19 @@ import ru.drudenko.alisa.model.Otp;
 import ru.drudenko.alisa.repository.ClientRepository;
 import ru.drudenko.alisa.repository.OtpRepository;
 
+import java.io.IOException;
 import java.io.StringWriter;
+import java.security.GeneralSecurityException;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/oauth", produces = MediaType.ALL_VALUE)
 public class OauthController {
 
-    @Value("${app.client_id}")
+    @Value("${app.yandex.client_id}")
     private String client_id;
 
-    @Value("${app.client_secret}")
+    @Value("${app.yandex.client_secret}")
     private String client_secret;
 
     private final RestTemplate yandexRestTemplate;
@@ -80,6 +94,14 @@ public class OauthController {
                 .body(writer.toString());
     }
 
+    @GetMapping(value = "/google", produces = {"text/html;charset=UTF-8"})
+    ResponseEntity google(@RequestParam(name = "state") String state, @RequestParam(name = "code") String code) {
+
+        System.out.println(code);
+        return ResponseEntity
+                .ok().build();
+    }
+
     private String getToken(String code) {
 
         MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
@@ -100,6 +122,51 @@ public class OauthController {
                         ParameterizedTypeReference.forType(Token.class));
 
         return responseEntity.getBody().getAccess_token();
+    }
+
+    private static final String APPLICATION_NAME = "Gmail API Java Quickstart";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+
+    public static void main(String[] args) throws GeneralSecurityException, IOException {
+        // Build a new authorized API client service.
+
+
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        OAuthParameters oAuthParameters = new OAuthParameters();
+        String token = "ya29.GluiBipkMmiRhj7BpgzPDpi3tnDVxJtXVROeYfYLgMR4BWatSES_y6OvCSwrgrMekYEjw7srSCt1_9Bc5CTo8g1q44NdUtUpMHpXaGF1YhT_2BjRg_XCkmdEJqOj";
+        Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, convertToGoogleCredential(token, "1/bVZQqRLjt0e_wS-4PtRjhiPUMSykoXm6yHLWoAjaTxY", "gykHUJOipdk1a5p-8A-1RYxy", "788764317534-erfn8p6im4v4vo5i2b28c6s4emqejlav.apps.googleusercontent.com"))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+
+        // Print the labels in the user's account.
+        String user = "me";
+        ListLabelsResponse listResponse = service.users().labels().list(user).execute();
+        List<Label> labels = listResponse.getLabels();
+        if (labels.isEmpty()) {
+            System.out.println("No labels found.");
+        } else {
+            System.out.println("Labels:");
+            for (Label label : labels) {
+                System.out.printf("- %s\n", label.getName());
+            }
+        }
+
+    }
+
+    private static Credential convertToGoogleCredential(String accessToken, String refreshToken, String apiSecret, String apiKey) {
+        HttpTransport httpTransport = new NetHttpTransport();
+        JsonFactory jsonFactory = new JacksonFactory();
+        GoogleCredential credential = new GoogleCredential.Builder().setTransport(httpTransport).setJsonFactory(jsonFactory).setClientSecrets(apiKey, apiSecret).build();
+        credential.setAccessToken(accessToken);
+        credential.setRefreshToken(refreshToken);
+        try {
+            credential.refreshToken();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return credential;
     }
 
 }
