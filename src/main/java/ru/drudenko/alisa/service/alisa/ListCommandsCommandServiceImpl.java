90761c66ffc9d998b900536040421f0357da0fdc
fso13 <dimarudenk@yandex.ru>
2019-02-03 15:53:38 +0300

    fix service list commands

--- src/main/java/ru/drudenko/alisa/service/alisa/ListCommandsCommandServiceImpl.java
+++ src/main/java/ru/drudenko/alisa/service/alisa/ListCommandsCommandServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.drudenko.alisa.service.alisa;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.drudenko.alisa.dto.dialog.req.Command;
 
 import java.util.Arrays;
@@ -8,6 +9,7 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+@Service
 public class ListCommandsCommandServiceImpl implements CommandService {
     private static final List<String> LIST_COMMANDS = Arrays.asList("список", "команд");
 
